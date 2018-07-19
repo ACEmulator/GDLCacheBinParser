637537dc54f13bc2b45f1d7a6b2c4766371440df
Mag-nus <Mag-nus@users.noreply.github.com>
2018-07-19 09:19:36 -0500

    Align the JSON package with what is used in the ACE dll's
    This also adds WeenieClassExtensions.GetNameFormattedForDatabase()

---
+++ Enums/WeenieClassesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PhatACCacheBinParser.Enums
+{
+    static class WeenieClassesExtensions
+    {
+        public static string GetNameFormattedForDatabase(this WeenieClasses weenieClass)
+        {
+            if (!Enum.IsDefined(typeof(WeenieClasses), weenieClass))
+                return "ace" + weenieClass;
+
+            var result = weenieClass.ToString();
+
+            // Remove the W_ at the front
+            if (result.StartsWith("W_"))
+                result = result.Substring(2);
+
+            // Remove the trailing _CLASS
+            if (result.EndsWith("_CLASS"))
+                result = result.Substring(0, result.Length - 6);
+
+            result = result.Replace("_", "-");
+
+            result = result.ToLower();
+
+            return result;
+        }
+    }
+}
