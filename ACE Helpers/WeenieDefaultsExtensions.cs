0baf3f06fdd57a637340d0f52c1cfae9e257661a
Ty Conner <tyconner@itcproductions.com>
2019-01-27 03:39:41 -0500

    Update to get a few more weenies out of cache bin.. they're mostly template stuff but not from pcaps so worth it

--- ACE Helpers/WeenieDefaultsExtensions.cs
+++ ACE Helpers/WeenieDefaultsExtensions.cs
@@ -19,9 +19,13 @@ namespace PhatACCacheBinParser.ACE_Helpers
                 // Highest Weenie Exported in WorldSpawns was: 30937
                 // Highest Weenie found in WeenieClasses was: 31034
 
-                uint highestWeenieAllowed = 31034;
+                uint lowestWeenieAllowed = 1;
+                uint highestWeenieAllowed = 31220; // previously 31034
 
-                if (value.Key > highestWeenieAllowed && highestWeenieAllowed > 0)
+                if (value.Key < lowestWeenieAllowed)
+                    continue;
+
+                if (value.Key > highestWeenieAllowed)
                     continue;
 
                 var converted = value.ConvertToACE();
