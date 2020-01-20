b50bc8db24fb3ccaa7dba1704304ed91716f493f
Ty Conner <tyconner@itcproductions.com>
2020-01-20 13:16:57 -0500

    Fix math error

--- ACE Helpers/RegionDescExtendedDataExtensions.cs
+++ ACE Helpers/RegionDescExtendedDataExtensions.cs
@@ -25,9 +25,9 @@ namespace PhatACCacheBinParser.ACE_Helpers
 
                 var terrain_base = landBlockData.TerrainLandblocks[tbIndex];
 
-                for (var cell_x = 0; cell_x < 8; cell_x++)
+                for (var cell_x = 0; cell_x < 9; cell_x++)
                 {
-                    for (var cell_y = 0; cell_y < 8; cell_y++)
+                    for (var cell_y = 0; cell_y < 9; cell_y++)
                     {
                         var terrain = terrain_base.Terrain[(cell_x * 9) + cell_y];
 
